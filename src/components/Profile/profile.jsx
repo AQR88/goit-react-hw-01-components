e3c28fb2e05e20460f88afcfe530a118a6ddb34d
AQR88 <timahaua@gmail.com>
2023-11-26 12:43:47 +0200

    Profile

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,34 +1,49 @@
+import user from './user.json';
 
-export default function userProfile (){
-    return (
-        <>
-        <div className="profile">
-  <div className="description">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      className="avatar"
-    />
-    <p className="name">Petra Marica</p>
-    <p className="tag">@pmarica</p>
-    <p className="location">Salvador, Brasil</p>
-  </div>
-  <ul className="stats">
-    <li>
-      <span className="label">Followers</span>
-      <span className="quantity">1000</span>
-    </li>
-    <li>
-      <span className="label">Views</span>
-      <span className="quantity">2000</span>
-    </li>
-    <li>
-      <span className="label">Likes</span>
-      <span className="quantity">3000</span>
-    </li>
-  </ul>
+function UserCard ({username,tag,location,avatar,stats}){
+  return (
+      <>
+      <div className="profile">
+<div className="description">
+  <img
+    src={avatar}
+    alt="User avatar"
+    className="avatar"
+  />
+  <p className="name">{username}</p>
+  <p className="tag">{tag}</p>
+  <p className="location">{location}</p>
 </div>
+<ul className="stats">
+  <li>
+    <span className="label">Follovers {stats.followers}</span>
+    <span className="quantity">{stats.followers.data}</span>
+  </li>
+  <li>
+    <span className="label">Views {stats.views}</span>
+    <span className="quantity">{stats.views.data}</span>
+  </li>
+  <li>
+    <span className="label">Stats {stats.likes}</span>
+    <span className="quantity">{stats.likes.data}</span>
+  </li>
+</ul>
+</div>
+    </>
+  )
+};
 
-        </>
-    )
-}
+
+export default function Profile (){
+  return(
+    <UserCard
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+    />
+  )
+};
+
+
