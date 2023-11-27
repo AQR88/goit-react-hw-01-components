3feeedef8d5ac964787534d8a0ff668a2c644d4b
AQR88 <timahaua@gmail.com>
2023-11-27 22:28:57 +0200

    final_comit

--- src/components/Statistics/statistic.jsx
+++ src/components/Statistics/statistic.jsx
@@ -1,5 +1,6 @@
 import data from './data.json';
 import css from './statistics.module.css';
+import getRandomColor from './statistics.js';
 
 function StatisticElem({ title, stats }) {
   return (
@@ -7,7 +8,11 @@ function StatisticElem({ title, stats }) {
       <h2 className={css.title}>{title}</h2>
       <ul className={css.list}>
         {stats.map(elem => (
-          <li className={css.item} key={elem.id}>
+          <li
+            className={css.item}
+            key={elem.id}
+            style={{ backgroundColor: getRandomColor() }}
+          >
             <span className={css.label}>{elem.label}</span>
             <span className={css.percentage}>{elem.percentage}%</span>
           </li>
--- src/components/TransactionHistory/transaction.jsx
+++ src/components/TransactionHistory/transaction.jsx
@@ -1,23 +1,27 @@
 import transactions from './transactions.json';
+import css from './transactions.module.css';
 
 function HistorySection({ items }) {
   return (
     <>
-      <table class="transaction-history">
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+      <table className={css.history}>
+        <thead className={css.head}>
+          <tr className={css.tr}>
+            <th className={css.title}>Type</th>
+            <th className={css.title}>Amount</th>
+            <th className={css.title}>Currency</th>
           </tr>
         </thead>
 
-        <tbody>
-          {items.map(({ id, type, amount, currency }) => (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
+        <tbody className={css.tbody}>
+          {items.map(({ id, type, amount, currency }, index) => (
+            <tr
+              key={id}
+              style={{ backgroundColor: index % 2 === 0 ? 'white' : '#c9c3c3' }}
+            >
+              <td className={css.td}>{type}</td>
+              <td className={css.td}>{amount}</td>
+              <td className={css.td}>{currency}</td>
             </tr>
           ))}
         </tbody>
